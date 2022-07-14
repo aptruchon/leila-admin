755886ab3e63659bff5d76fdec5f51ef67ea0535
aptruchon <aptruchon@gmail.com>
2022-07-14 09:25:20 -0400

    leila-admin

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+// URL de base du serveur
+const BASE_SERVEUR = "http://localhost/PWA/leila-admin/";
+
+//Config de la BD
+const BD_HOTE = "localhost";
+const BD_UTIL = "leilaadmin";
+const BD_MDP = "9IAlj2SYJ5wVlVBE";
+const BD_NOM = "leila";
+
+// Module pas défaut
+const MODULE_DEFAUT = "utilisateur";
---
+++ gabarits/HtmlGabarit.cls.php
@@ -0,0 +1,38 @@
+<?php
+use \Twig\Environment;
+use \Twig\Loader\FilesystemLoader;
+
+class HtmlGabarit
+{
+    protected $variables = array();
+    protected $module;
+    protected $action;
+    private $twig;
+
+    function __construct($module, $action)
+    {
+        $this->module = $module;
+        $this->action = $action;
+        $this->twig = new Environment(new FilesystemLoader(["vues/"]), []);
+    }
+
+    public function affecter($nom, $valeur)
+    {
+        $this->variables[$nom] = $valeur;
+    }
+
+    public function affecterActionParDefaut($nomAction)
+    {
+        $this->action = $nomAction;
+    }
+
+    public function genererVue()
+    {
+        /* extract($this->variables); // Voir la documentation 
+        include("vues/entete.inc.php");
+        include("vues/$this->module.$this->action.php");
+        include("vues/pied2page.inc.php"); */
+
+        $this->twig->display("$this->module.$this->action.twig", $this->variables);
+    }
+}
